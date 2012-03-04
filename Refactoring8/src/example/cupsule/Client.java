182a924120e9b727187a91ae33cf7f2a74762f84
urara <oura036@gmail.com>
2012-03-04 21:42:18 +0900

    start move

--- Refactoring8/src/example/cupsule/Client.java
+++ Refactoring8/src/example/cupsule/Client.java
@@ -1,10 +1,13 @@
 package example.cupsule;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+
 
 public class Client {
 	public static void main(String[] args){
+		//cupsule
 		Person kent = new Person();
 		Set s = new HashSet();
 //		s.add(new Course("Smalltalkプログラミング", false));
@@ -17,7 +20,18 @@ public class Client {
 		s.add(refact);
 		s.add(new Course("抽象的な要件の実現", true));
 		System.out.println(kent.getCources().size());
-		kent.getCources().remove(refact);
+		//kent.getCources().remove(refact);
+		kent.removeCourse(refact);
 		System.out.println(kent.getCources().size());
+		
+		//move Behavior
+		Iterator iter = kent.getCources().iterator();
+		int count = 0;
+		while(iter.hasNext()){
+			Course each = (Course) iter.next();
+			System.out.println(each.name);
+			if(each.isAdvanced()) count++;
+		}
+		
 	}
 }
